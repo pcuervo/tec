08fb25de4eae27cfc2d440e023fa86375117dce9
raul <raul@zamacona.me>
2015-03-10 23:10:15 -0600

    conflictos

--- themes/tec/footer.php
+++ themes/tec/footer.php
@@ -9,156 +9,162 @@
 			<div class="[ modal modal--transparent ]">
 				<div class="[ modal-content ]">
 					<div class="[ modal-body ]">
-						<a href="" class="[ block ][ columna xmall-12 ][ button button--rounded-corners ][ margin-bottom--small ][ bg-facebook ]">
-							<span class="[ ]">Ingresa con facebook</span>
-							<i class="[ icon-facebook ]"></i>
-						</a>
+						<article class="[ step-1 ]">
+							<a href="" class="[ block ][ columna xmall-12 ][ button button--rounded-corners ][ margin-bottom--small ][ bg-facebook ]">
+								<span class="[ ]">Ingresa con facebook</span>
+								<i class="[ icon-facebook ]"></i>
+							</a>
+						</article><!-- step-1 -->
 						<div class="clear"></div>
-						<form action="" class="[ forma forma-tu-historia ]">
-							<fieldset class="[ margin-bottom--small ]">
-								<div class="[ formplate ]">
-									<!-- <label for="nombre">Tu nombre</label> -->
-									<input type="text" name="nombre" placeholder="Tu nombre">
-								</div><!-- formplate -->
-								<div class="[ formplate ]">
-									<select name="campus" id="campus">
-										<option selected="true" disabled="disabled">Campus</option>
-										<option value="Aguascalientes">Aguascalientes</option>
-										<option value="Central de Veracruz">Central de Veracruz</option>
-										<option value="Chiapas">Chiapas</option>
-										<option value="Chihuahua">Chihuahua</option>
-										<option value="Ciudad de México">Ciudad de México</option>
-										<option value="Ciudad Juárez">Ciudad Juárez</option>
-										<option value="Ciudad Obregón">Ciudad Obregón</option>
-										<option value="Cuernavaca">Cuernavaca</option>
-										<option value="Estado de México">Estado de México</option>
-										<option value="Guadalajara">Guadalajara</option>
-										<option value="Hidalgo">Hidalgo</option>
-										<option value="Irapuato">Irapuato</option>
-										<option value="Laguna">Laguna</option>
-										<option value="León">León</option>
-										<option value="Monterrey">Monterrey</option>
-										<option value="Morelia">Morelia</option>
-										<option value="Puebla">Puebla</option>
-										<option value="Querétaro">Querétaro</option>
-										<option value="Saltillo">Saltillo</option>
-										<option value="San Luis Potosí">San Luis Potosí</option>
-										<option value="Santa Fe">Santa Fe</option>
-										<option value="Sinaloa">Sinaloa</option>
-										<option value="Sonora Norte">Sonora Norte</option>
-										<option value="Tampico">Tampico</option>
-										<option value="Toluca">Toluca</option>
-										<option value="Zacatecas">Zacatecas</option>
-									</select>
-								</div><!-- formplate -->
-								<div class="[ formplate ][ generacion ]">
-									<select name="generacion" id="campus">
-										<option selected="true" disabled="disabled">Generación</option>
-										<option value="1947">1947</option>
-										<option value="1948">1948</option>
-										<option value="1949">1949</option>
-										<option value="1950">1950</option>
-										<option value="1951">1951</option>
-										<option value="1952">1952</option>
-										<option value="1953">1953</option>
-										<option value="1954">1954</option>
-										<option value="1955">1955</option>
-										<option value="1956">1956</option>
-										<option value="1957">1957</option>
-										<option value="1958">1958</option>
-										<option value="1959">1959</option>
-										<option value="1960">1960</option>
-										<option value="1961">1961</option>
-										<option value="1962">1962</option>
-										<option value="1963">1963</option>
-										<option value="1964">1964</option>
-										<option value="1965">1965</option>
-										<option value="1966">1966</option>
-										<option value="1967">1967</option>
-										<option value="1968">1968</option>
-										<option value="1969">1969</option>
-										<option value="1970">1970</option>
-										<option value="1971">1971</option>
-										<option value="1972">1972</option>
-										<option value="1973">1973</option>
-										<option value="1974">1974</option>
-										<option value="1975">1975</option>
-										<option value="1976">1976</option>
-										<option value="1977">1977</option>
-										<option value="1978">1978</option>
-										<option value="1979">1979</option>
-										<option value="1980">1980</option>
-										<option value="1981">1981</option>
-										<option value="1982">1982</option>
-										<option value="1983">1983</option>
-										<option value="1984">1984</option>
-										<option value="1985">1985</option>
-										<option value="1986">1986</option>
-										<option value="1987">1987</option>
-										<option value="1988">1988</option>
-										<option value="1989">1989</option>
-										<option value="1990">1990</option>
-										<option value="1991">1991</option>
-										<option value="1992">1992</option>
-										<option value="1993">1993</option>
-										<option value="1994">1994</option>
-										<option value="1995">1995</option>
-										<option value="1996">1996</option>
-										<option value="1997">1997</option>
-										<option value="1998">1998</option>
-										<option value="1999">1999</option>
-										<option value="2000">2000</option>
-										<option value="2001">2001</option>
-										<option value="2002">2002</option>
-										<option value="2003">2003</option>
-										<option value="2004">2004</option>
-										<option value="2005">2005</option>
-										<option value="2006">2006</option>
-										<option value="2007">2007</option>
-										<option value="2008">2008</option>
-										<option value="2008">2008</option>
-										<option value="2009">2009</option>
-										<option value="2010">2010</option>
-										<option value="2011">2011</option>
-										<option value="2012">2012</option>
-										<option value="2013">2013</option>
-										<option value="2014">2014</option>
-										<option value="2015">2015</option>
-										<option value="2016">2016</option>
-										<option value="2017">2017</option>
-										<option value="2018">2018</option>
-										<option value="2019">2019</option>
-										<option value="2020">2020</option>
-									</select>
-								</div><!-- formplate -->
-							</fieldset>
-							<fieldset class="[ margin-bottom--small ]">
-								<div class="[ formplate ]">
-									<textarea name="historia" id="" placeholder="Tu historia"></textarea>
-								</div><!-- formplate -->
-							</fieldset>
-							<fieldset class="[ margin-bottom--small ]">
-								<div class="[ formplate ]">
-									<textarea name="titulo" id="" placeholder="Quote"></textarea>
-								</div><!-- formplate -->
-							</fieldset>
-							<fieldset class="[ margin-bottom--small ]">
-								<input class="[ block ][ columna xmall-12 ]" type="file" value="Agregar imagen a mi historia">
-							</fieldset>
-							<fieldset class="[ margin-bottom--small ]">
-								<div class="[ formplate ]">
-									<input class="toggler" name="acepto" value="toggler" type="checkbox">
-									<p class="[ text-center ]">Deseo que mi historia se publique en mi muro de Facebook.</p>
-								</div><!-- formplate -->
-							</fieldset>
-							<fieldset class="[ margin-bottom--small ]">
-								<button class="[ block ][ columna xmall-12 ][ button button--rounded-corners ]" type="submit">Enviar</button>
-							</fieldset>
-						</form>
-						<div class="[ success-message ]">
-							<p class="[ title ][ text-center ][ margin-bottom--large ][ light ]">Gracias por compartir tu historia con México</p>
-							<p class="[ text-center ][ light ]">Tu historia será revisada por nosotros y se te notificará en cuanto esté aprobada.</p>
-						</div>
+						<article class="[ step-2 ]">
+							<form action="" class="[ forma forma-tu-historia ]">
+								<fieldset class="[ margin-bottom--small ]">
+									<div class="[ formplate ]">
+										<!-- <label for="nombre">Tu nombre</label> -->
+										<input type="text" name="nombre" placeholder="Tu nombre">
+									</div><!-- formplate -->
+									<div class="[ formplate ]">
+										<select name="campus" id="campus">
+											<option selected="true" disabled="disabled">Campus</option>
+											<option value="Aguascalientes">Aguascalientes</option>
+											<option value="Central de Veracruz">Central de Veracruz</option>
+											<option value="Chiapas">Chiapas</option>
+											<option value="Chihuahua">Chihuahua</option>
+											<option value="Ciudad de México">Ciudad de México</option>
+											<option value="Ciudad Juárez">Ciudad Juárez</option>
+											<option value="Ciudad Obregón">Ciudad Obregón</option>
+											<option value="Cuernavaca">Cuernavaca</option>
+											<option value="Estado de México">Estado de México</option>
+											<option value="Guadalajara">Guadalajara</option>
+											<option value="Hidalgo">Hidalgo</option>
+											<option value="Irapuato">Irapuato</option>
+											<option value="Laguna">Laguna</option>
+											<option value="León">León</option>
+											<option value="Monterrey">Monterrey</option>
+											<option value="Morelia">Morelia</option>
+											<option value="Puebla">Puebla</option>
+											<option value="Querétaro">Querétaro</option>
+											<option value="Saltillo">Saltillo</option>
+											<option value="San Luis Potosí">San Luis Potosí</option>
+											<option value="Santa Fe">Santa Fe</option>
+											<option value="Sinaloa">Sinaloa</option>
+											<option value="Sonora Norte">Sonora Norte</option>
+											<option value="Tampico">Tampico</option>
+											<option value="Toluca">Toluca</option>
+											<option value="Zacatecas">Zacatecas</option>
+										</select>
+									</div><!-- formplate -->
+									<div class="[ formplate ][ generacion ]">
+										<select name="generacion" id="campus">
+											<option selected="true" disabled="disabled">Generación</option>
+											<option value="1947">1947</option>
+											<option value="1948">1948</option>
+											<option value="1949">1949</option>
+											<option value="1950">1950</option>
+											<option value="1951">1951</option>
+											<option value="1952">1952</option>
+											<option value="1953">1953</option>
+											<option value="1954">1954</option>
+											<option value="1955">1955</option>
+											<option value="1956">1956</option>
+											<option value="1957">1957</option>
+											<option value="1958">1958</option>
+											<option value="1959">1959</option>
+											<option value="1960">1960</option>
+											<option value="1961">1961</option>
+											<option value="1962">1962</option>
+											<option value="1963">1963</option>
+											<option value="1964">1964</option>
+											<option value="1965">1965</option>
+											<option value="1966">1966</option>
+											<option value="1967">1967</option>
+											<option value="1968">1968</option>
+											<option value="1969">1969</option>
+											<option value="1970">1970</option>
+											<option value="1971">1971</option>
+											<option value="1972">1972</option>
+											<option value="1973">1973</option>
+											<option value="1974">1974</option>
+											<option value="1975">1975</option>
+											<option value="1976">1976</option>
+											<option value="1977">1977</option>
+											<option value="1978">1978</option>
+											<option value="1979">1979</option>
+											<option value="1980">1980</option>
+											<option value="1981">1981</option>
+											<option value="1982">1982</option>
+											<option value="1983">1983</option>
+											<option value="1984">1984</option>
+											<option value="1985">1985</option>
+											<option value="1986">1986</option>
+											<option value="1987">1987</option>
+											<option value="1988">1988</option>
+											<option value="1989">1989</option>
+											<option value="1990">1990</option>
+											<option value="1991">1991</option>
+											<option value="1992">1992</option>
+											<option value="1993">1993</option>
+											<option value="1994">1994</option>
+											<option value="1995">1995</option>
+											<option value="1996">1996</option>
+											<option value="1997">1997</option>
+											<option value="1998">1998</option>
+											<option value="1999">1999</option>
+											<option value="2000">2000</option>
+											<option value="2001">2001</option>
+											<option value="2002">2002</option>
+											<option value="2003">2003</option>
+											<option value="2004">2004</option>
+											<option value="2005">2005</option>
+											<option value="2006">2006</option>
+											<option value="2007">2007</option>
+											<option value="2008">2008</option>
+											<option value="2008">2008</option>
+											<option value="2009">2009</option>
+											<option value="2010">2010</option>
+											<option value="2011">2011</option>
+											<option value="2012">2012</option>
+											<option value="2013">2013</option>
+											<option value="2014">2014</option>
+											<option value="2015">2015</option>
+											<option value="2016">2016</option>
+											<option value="2017">2017</option>
+											<option value="2018">2018</option>
+											<option value="2019">2019</option>
+											<option value="2020">2020</option>
+										</select>
+									</div><!-- formplate -->
+								</fieldset>
+								<fieldset class="[ margin-bottom--small ]">
+									<div class="[ formplate ]">
+										<textarea name="historia" id="" placeholder="Tu historia"></textarea>
+									</div><!-- formplate -->
+								</fieldset>
+								<fieldset class="[ margin-bottom--small ]">
+									<div class="[ formplate ]">
+										<textarea name="titulo" id="" placeholder="Quote"></textarea>
+									</div><!-- formplate -->
+								</fieldset>
+								<fieldset class="[ margin-bottom--small ]">
+									<input class="[ block ][ columna xmall-12 ]" type="file" value="Agregar imagen a mi historia">
+								</fieldset>
+								<fieldset class="[ margin-bottom--small ]">
+									<div class="[ formplate ]">
+										<input class="toggler" name="acepto" value="toggler" type="checkbox">
+										<p class="[ text-center ]">Deseo que mi historia se publique en mi muro de Facebook.</p>
+									</div><!-- formplate -->
+								</fieldset>
+								<fieldset class="[ margin-bottom--small ]">
+									<button class="[ block ][ columna xmall-12 ][ button button--rounded-corners ]" type="submit">Enviar</button>
+								</fieldset>
+							</form>
+						</article><!-- step-2 -->
+						<article class="[ step-3 ]">
+							<div class="[ success-message ]">
+								<p class="[ title ][ text-center ][ margin-bottom--large ][ light ]">Gracias por compartir tu historia con México</p>
+								<p class="[ text-center ][ light ]">Tu historia será revisada por nosotros y se te notificará en cuanto esté aprobada.</p>
+							</div>
+						</article><!-- step-2 -->
 					</div><!-- modal-body -->
 				</div><!-- modal-content -->
 			</div>
