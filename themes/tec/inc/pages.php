67ab80429d692f7dc5b77a6feaa92e5d6452bd03
raul <raul@zamacona.me>
2015-03-24 19:26:47 -0600

    privacidad

--- themes/tec/inc/pages.php
+++ themes/tec/inc/pages.php
@@ -8,16 +8,16 @@
 
 
 		// CONTACTO
-		/*if( ! get_page_by_path('faq') ){
+		if( ! get_page_by_path('privacidad') ){
 			$page = array(
 				'post_author' => 1,
 				'post_status' => 'publish',
-				'post_title'  => 'Frequently Asked Questions',
-				'post_name'   => 'faq'
+				'post_title'  => 'Privacidad',
+				'post_name'   => 'privacidad',
 				'post_type'   => 'page'
 			);
 			wp_insert_post( $page, true );
-		}*/
+		}
 
 
 	});
---
+++ themes/tec/page-privacidad.php
@@ -0,0 +1,15 @@
+<?php
+	get_header();
+	the_post();
+?>
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<br />
+	<section class="[ wrapper ][ bg-light ][ padding ]">
+		<h5 class="[ display-text display-text--medium display-text--bold ][ sub-sub-title ]">Avíso de privacidad</h5>
+		<?php the_content( ); ?>
+	</section><!-- wrapper -->
+<?php get_footer(); ?>
